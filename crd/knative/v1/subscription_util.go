f70adc721e642e2435c5f5ba2daef6eed5ac8658
p_pxxtang <p_pxxtang@tencent.com>
2022-04-25 17:43:51 +0800

    feature(subscription): 实现subscription runner

---
+++ crd/knative/v1/subscription_util.go
@@ -0,0 +1,52 @@
+package v1
+
+import (
+	"fmt"
+)
+
+func (in Subscriber) String() string {
+	if in.Uri != nil {
+		return fmt.Sprintf(`URI:%s`, in.Uri.String())
+	}
+	if in.Service != nil {
+		return fmt.Sprintf("SERVICE:", in.Service.String())
+	}
+	if in.Pod != nil {
+		return fmt.Sprintf("POD:%s", in.Pod.String())
+	}
+	panic("Unknown Subscriber type")
+}
+
+func (in SubscriberUri) String() string {
+	return fmt.Sprintf(`%s://%s`, in.Protocol, in.Uri)
+}
+
+func (in SubscriberService) String() string {
+	return fmt.Sprintf("%s/%s", in.Namespace, in.Name)
+}
+
+func (in SubscriberPod) String() string {
+	return fmt.Sprintf("%s/%s", in.Namespace, in.Selector.String())
+}
+
+func (in Subscriber) GetTarget() string {
+	if in.Uri != nil {
+		return in.Uri.GetTarget()
+	}
+	if in.Service != nil {
+		return in.Service.GetTarget()
+	}
+	//TODO:实现pod的target
+	//if in.Pod != nil {
+	//	return in.Pod.String()
+	//}
+	panic("Unknown Subscriber type")
+}
+
+func (in SubscriberUri) GetTarget() string {
+	return in.String()
+}
+
+func (in SubscriberService) GetTarget() string {
+	return fmt.Sprintf(`http://%s.%s:%d/%s`, in.Name, in.Namespace, in.Port, in.Path)
+}
--- go.mod
+++ go.mod
@@ -10,14 +10,19 @@ require (
 )
 
 require (
+	github.com/cloudevents/sdk-go/v2 v2.8.0 // indirect
 	github.com/go-logr/logr v1.2.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/google/uuid v1.1.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0 // indirect
 	golang.org/x/net v0.0.0-20210825183410-e898025ed96a // indirect
 	golang.org/x/sys v0.0.0-20211205182925-97ca703d548d // indirect
 	golang.org/x/text v0.3.7 // indirect
